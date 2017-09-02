7fcb139049c5e06ea330e03d726739e08ec84cdd
Paul Hammant <paul@hammant.org>
2017-09-02 11:48:31 -0400

    check contents of bundle, too

--- shelve_test.py
+++ shelve_test.py
@@ -1,5 +1,6 @@
 import fileinput
 import os
+import glob2
 import shutil
 import tempfile
 import zipfile
@@ -10,8 +11,6 @@ svn_shelve = __import__("svn-shelve")
 
 
 def test_answer():
-
-    print "good 1"
 
 
     sh.rm("-rf", "maven-gpg-plugin-WC")
@@ -38,7 +37,17 @@ def test_answer():
     sh.cd(tmpdir + "/foo.stash")
     log = sh.git("log", "--pretty=oneline", "--no-color", _tty_out=False).splitlines()
 
+    files = str(glob2.glob("maven-gpg-plugin-WC/**/*"))
+
     shutil.rmtree(tmpdir)
+
+    assert files == "['maven-gpg-plugin-WC/pom.xml', 'maven-gpg-plugin-WC/pom.xml.info', 'maven-gpg-plugin-WC/src', " \
+                    "'maven-gpg-plugin-WC/src/main', 'maven-gpg-plugin-WC/src/main/java', 'maven-gpg-plugin-WC/src/main/java/org', " \
+                    "'maven-gpg-plugin-WC/src/main/java/org/apache', 'maven-gpg-plugin-WC/src/main/java/org/apache/maven', " \
+                    "'maven-gpg-plugin-WC/src/main/java/org/apache/maven/plugin', " \
+                    "'maven-gpg-plugin-WC/src/main/java/org/apache/maven/plugin/gpg', " \
+                    "'maven-gpg-plugin-WC/src/main/java/org/apache/maven/plugin/gpg/SigningBundle.java', " \
+                    "'maven-gpg-plugin-WC/src/main/java/org/apache/maven/plugin/gpg/SigningBundle.java.info']"
 
     assert len(log) == 2
     assert log[0].endswith(" finish")
